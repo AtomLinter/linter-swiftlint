852429028f2b4a16d7636f9a522b16f3f333d84c
Kepler Sticka-Jones <kepler+git@stickajones.org>
2018-04-17 17:22:19 -0600

    refactor: stricter typing for atom-linter module

--- src/types/atom-linter.d.ts
+++ src/types/atom-linter.d.ts
@@ -1,61 +1,61 @@
 declare module "atom-linter" {
   import { Message } from "atom/linter";
-  import { TextEditor } from "atom";
+  import { TextEditor, Range } from "atom";
 
   export const FindCache: Map<any, any>;
 
   export function exec(
-    command: String,
+    command: string,
     args: Array<string>,
     options: Object
-  ): Promise<String>;
+  ): Promise<string>;
 
   export function execNode<T>(
-    filePath: String,
+    filePath: string,
     args: Array<string>,
     options: Object
   ): Promise<T>;
 
   export function parse(
-    data: String,
-    regex: String,
-    options: Object
+    data: string,
+    regex: string,
+    options: object
   ): Array<Message>;
 
   export function generateRange<T>(
     textEditor: TextEditor,
-    lineNumber: Number,
-    colStart: Number
-  ): Array<T>;
+    lineNumber: number,
+    colStart: number
+  ): Array<Range>;
 
   export function find(
-    directory: String,
-    names: String | Array<string>
-  ): String | null;
+    directory: string,
+    names: string | Array<string>
+  ): string | null;
 
   export function findCached(
-    directory: String,
-    names: String | Array<string>
-  ): String | null;
+    directory: string,
+    names: string | Array<string>
+  ): string | null;
 
   export function findAsync(
-    directory: String,
-    names: String | Array<string>
-  ): Promise<String | null>;
+    directory: string,
+    names: string | Array<string>
+  ): Promise<string | null>;
 
   export function findCachedAsync(
-    directory: String,
-    names: String | Array<string>
-  ): Promise<String | null>;
+    directory: string,
+    names: string | Array<string>
+  ): Promise<string | null>;
 
   export function tempFile<T>(
-    fileName: String,
-    fileContents: String,
+    fileName: string,
+    fileContents: string,
     callback: Function
   ): Promise<T>;
 
   export function tempFiles<T>(
-    filesNames: Array<{ name: String; contents: String }>,
+    filesNames: Array<{ name: string; contents: string }>,
     callback: Function
   ): Promise<T>;
 }
