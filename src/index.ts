6e691e81bd922fa19064a93c0da82dcbc14ea6c3
Landon Abney <Landonabney@gmail.com>
2018-04-17 23:00:58 -0700

    fix(performance): install dependencies in an idle callback
    Defer checking for and installing Atom package dependencies into an idle
    callback so Atom's initialization of the package isn't delayed.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,48 @@
 import { LinterProvider } from "atom/linter";
 
+// TypeScript apparently has absolutely zero support for technologies marked
+// experimental, so this has to be declared manually.
+// https://github.com/Microsoft/TypeScript/issues/21309
+type RequestIdleCallbackHandle = any;
+type RequestIdleCallbackOptions = {
+  timeout: number;
+};
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean;
+  timeRemaining: (() => number);
+};
+
+declare global {
+  interface Window {
+    requestIdleCallback: ((
+      callback: ((deadline: RequestIdleCallbackDeadline) => void),
+      opts?: RequestIdleCallbackOptions,
+    ) => RequestIdleCallbackHandle);
+    cancelIdleCallback: ((handle: RequestIdleCallbackHandle) => void);
+  }
+}
+
+// Internal variables
+const idleCallbacks = new Set();
+
+const makeIdleCallback = (work: any) => {
+  let callbackId: RequestIdleCallbackHandle;
+  const callBack = () => {
+    idleCallbacks.delete(callbackId)
+    work()
+  }
+  callbackId = window.requestIdleCallback(callBack)
+  idleCallbacks.add(callbackId)
+}
+
 class SwiftLintLinter {
   public activate(): void {
-    require("atom-package-deps").install("linter-swiftlint");
+    const linterSwiftLintInstallPeerPackages = () => {
+      require("atom-package-deps").install("linter-swiftlint");
+    }
+    if (!atom.inSpecMode()) {
+      makeIdleCallback(linterSwiftLintInstallPeerPackages);
+    }
   }
 
   public provideLinter(): LinterProvider {
